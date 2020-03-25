9de4c4e73f0eedf4ac6189e0de6e7821cff3bbd1
piranna <piranna@users.noreply.github.com>
2020-03-25 01:58:19 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.9808c3632f4708330e553458e050af7d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c74b399c4a3ace587b9ba83eaa4f798",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bb9b48fbb19a42220ed",
+    "url": "/static/css/main.72a20339.chunk.css"
+  },
+  {
+    "revision": "f8820be6c18fde7a6a5c",
+    "url": "/static/js/2.9a32696f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.9a32696f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bb9b48fbb19a42220ed",
+    "url": "/static/js/main.1a12c349.chunk.js"
+  },
+  {
+    "revision": "23f44f103562d37daa7b",
+    "url": "/static/js/runtime-main.4ef43e80.js"
+  },
+  {
+    "revision": "b1abb878e2529cb5cb4450139844155d",
+    "url": "/static/media/AvenirLTStd-Black.b1abb878.otf"
+  },
+  {
+    "revision": "ecb0c2ae369ba2a89d9a1ec2a1b3187b",
+    "url": "/static/media/AvenirLTStd-Book.ecb0c2ae.otf"
+  },
+  {
+    "revision": "a7edaaca7240679d0cda0cce2c2e896e",
+    "url": "/static/media/AvenirLTStd-Heavy.a7edaaca.otf"
+  },
+  {
+    "revision": "0b78ec9d509f67bfe3f8458c9d285df0",
+    "url": "/static/media/AvenirLTStd-Light.0b78ec9d.otf"
+  },
+  {
+    "revision": "4f995fa49446998983e05df9994dc96c",
+    "url": "/static/media/AvenirLTStd-Medium.4f995fa4.otf"
+  },
+  {
+    "revision": "b1d7c6e085a31e9f5e4745c9aef6eb4b",
+    "url": "/static/media/AvenirLTStd-Roman.b1d7c6e0.otf"
+  },
+  {
+    "revision": "b2170ed5f30a19c1e9c0e67634a5aa01",
+    "url": "/static/media/morty.b2170ed5.png"
+  },
+  {
+    "revision": "ce1a0d8eaf4be605436a2852a5b4d1c8",
+    "url": "/static/media/rick.ce1a0d8e.png"
+  }
+]);
