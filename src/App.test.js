2f57e6906a883e787b1bbe178b1e5caed20683f2
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2020-03-25 02:50:49 +0100

    Fixed smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders RICKLEAPP', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/RICKLEAPP/i);
   expect(linkElement).toBeInTheDocument();
 });
